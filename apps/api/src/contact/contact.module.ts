85db3a4a9cb894e93cda70c281564709756812d7
Sherwyn Joel <sherwynjoel2004@email.com>
2026-04-01 20:35:46 +0530

    fix: provide PrismaService in ContactModule

--- apps/api/src/contact/contact.module.ts
+++ apps/api/src/contact/contact.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ContactController } from './contact.controller';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [ContactController],
+  providers: [PrismaService],
 })
 export class ContactModule {}
